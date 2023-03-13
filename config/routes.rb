05be1d3b473d5b03bc2d2005b74ecb7b1af4a3cb
Braden <braden.clokie@gmail.com>
2023-03-13 19:57:39 -0400

    Added user authentication functionality and fixed /about routing

--- config/routes.rb
+++ config/routes.rb
@@ -12,13 +12,22 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
-  resources :about
+  resources :about, only: [:index]
 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create]
   end
+
+  resources :users, only: [:new, :create]
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create'
+
+  resources :sessions, only: [:new, :create, :destroy]
+    get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
+    get '/logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
